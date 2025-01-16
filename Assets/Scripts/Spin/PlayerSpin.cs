e42d57b3f7510332307b40233ebdc3171a33601c
Noiverre <83685442+noiverree@users.noreply.github.com>
2025-01-16 23:52:55 +0700

    Fix rigidbody when timescale changed

--- Assets/Scripts/Spin/PlayerSpin.cs
+++ Assets/Scripts/Spin/PlayerSpin.cs
@@ -6,9 +6,29 @@ public class PlayerSpin : MonoBehaviour
 {
     private Transform spinnable;
 
+    [SerializeField]
+    private int abilityBars = 3;
+
+    [SerializeField]
+    private float timeWindow = 3f;
+
+    private bool isSpinning;
+
+    private Coroutine abilityWindow;
+
     private void Update()
     {
-        if (Input.GetKey(KeyCode.Mouse0))
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            EnterAbility();
+        }
+
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            ExitAbility();
+        }
+
+        if (Input.GetKey(KeyCode.Mouse0) && isSpinning)
         {
             Vector3 worldMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Vector3 direction = worldMousePosition - transform.position;
@@ -37,5 +57,33 @@ public class PlayerSpin : MonoBehaviour
         {
             spinnable.GetComponent<ISpin>().Spin(direction);
         }
+    }
+
+    private void EnterAbility()
+    {
+        Time.timeScale = 0.1f;
+        isSpinning = true;
+        abilityWindow = StartCoroutine(AbilityWindow());
+    }
+
+    private IEnumerator AbilityWindow()
+    {
+        yield return new WaitForSecondsRealtime(timeWindow);
+
+        ExitAbility();
+    }
+
+    private void ExitAbility()
+    {
+        if (abilityWindow != null)
+        {
+            StopCoroutine(abilityWindow);
+        }
+
+        isSpinning = false;
+
+        Time.timeScale = 1f;
+
+        spinnable = null;
     }
 }
--- Assets/Scripts/Spin/SpinnableBase.cs
+++ Assets/Scripts/Spin/SpinnableBase.cs
@@ -8,6 +8,8 @@ public class SpinnableBase : MonoBehaviour, ISpin
     private bool isChild;
     [SerializeField]
     private List<float> possibleAngles = new List<float> { 0, 90, 180, 270, -90, -180, -270 };
+    [SerializeField]
+    private Collider2D platformCollider;
 
     public void Spin(Vector3 spinDirection)
     {
@@ -31,6 +33,7 @@ public class SpinnableBase : MonoBehaviour, ISpin
             }
 
             transform.parent.rotation = Quaternion.Euler(0, 0, closestAngle);
+            ChangeEffector(closestAngle);
         }
 
         else
@@ -51,6 +54,21 @@ public class SpinnableBase : MonoBehaviour, ISpin
             }
 
             transform.rotation = Quaternion.Euler(0, 0, closestAngle);
+            ChangeEffector(closestAngle);
+        }
+    }
+
+    private void ChangeEffector(float angle)
+    {
+        if (angle == 0 || angle == 180 || angle == -180)
+        {
+            platformCollider.usedByEffector = true;
+            platformCollider.transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+
+        else if (angle == 90 || angle == -270 || angle == 270 || angle == -90)
+        {
+            platformCollider.usedByEffector = false;
         }
     }
 }
